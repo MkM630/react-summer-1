cb33f78da87af15fe885fd75bfdbb4b20baff534
MkM630 <mainakmaitra33@gmail.com>
2024-04-30 12:42:26 +0530

    add:deploy workflow-2

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 const Navigation = () =>{
     return( <nav>
         <div className="logo">
-            <img src="/images/brand_logo.png" alt="logo" />
+            <img src="./images/brand_logo.png" alt="logo" />
         </div>
 
     <ul>
